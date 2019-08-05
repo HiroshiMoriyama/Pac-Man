da5933eadb5d2de3f6cd39d59a0bfc7cd1c26e3f
Hiroshi Moriyama <imet8335on2000@gmail.com>
2019-08-05 12:10:22 +0900

    NevMesh機能追加 テスト中

--- Assets/Enemy/Test.cs
+++ Assets/Enemy/Test.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyController : MonoBehaviour
+public class Test : MonoBehaviour
 {
     public Transform skull;
     NavMeshAgent agent;
