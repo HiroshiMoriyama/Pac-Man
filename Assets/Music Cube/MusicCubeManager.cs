96a8169e8f4a2b2656968e320f17044eaf35bad3
Hiroshi Moriyama <imet8335on2000@gmail.com>
2019-08-06 15:48:46 +0900

    ステージ座標変更

--- Assets/Music Cube/MusicCubeManager.cs
+++ Assets/Music Cube/MusicCubeManager.cs
@@ -11,31 +11,36 @@ public class MusicCubeManager : MonoBehaviour
     public GameObject cubeH;
 
     public float MinRange;
-    public float MaxRange;
+    public float MaxRangeX;
+    public float MaxRangeZ;
     public int cubeNum;
 
     void Start()
     {
+        object[,] cubePos = new object[25, 25];
+
+        cubePos[0, 0] = Instantiate(cubeFsharp);
+
         for (int i = 1; i <= cubeNum; i++)
         {
-            Instantiate(cubeD, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
-                Random.Range(MinRange, MaxRange)),
+            Instantiate(cubeD, new Vector3(Random.Range(MinRange, MaxRangeX), 0.4f,
+                Random.Range(MinRange, MaxRangeZ)),
                 Quaternion.identity);
 
-            Instantiate(cubeE, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
-                Random.Range(MinRange, MaxRange)),
+            Instantiate(cubeE, new Vector3(Random.Range(MinRange, MaxRangeX), 0.4f,
+                Random.Range(MinRange, MaxRangeZ)),
                 Quaternion.identity);
 
-            Instantiate(cubeFsharp, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
-                Random.Range(MinRange, MaxRange)),
+            Instantiate(cubeFsharp, new Vector3(Random.Range(MinRange, MaxRangeX), 0.4f,
+                Random.Range(MinRange, MaxRangeZ)),
                 Quaternion.identity);
 
-            Instantiate(cubeA, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
-                Random.Range(MinRange, MaxRange)),
+            Instantiate(cubeA, new Vector3(Random.Range(MinRange, MaxRangeX), 0.4f,
+                Random.Range(MinRange, MaxRangeZ)),
                 Quaternion.identity);
 
-            Instantiate(cubeH, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
-                Random.Range(MinRange, MaxRange)),
+            Instantiate(cubeH, new Vector3(Random.Range(MinRange, MaxRangeX), 0.4f,
+                Random.Range(MinRange, MaxRangeZ)),
                 Quaternion.identity);
         }
     }
