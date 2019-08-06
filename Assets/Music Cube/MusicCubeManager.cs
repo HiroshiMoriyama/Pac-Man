7fca44cfbfacea7dd87f5a820b279fe477c9f390
Hiroshi Moriyama <imet8335on2000@gmail.com>
2019-08-06 11:59:29 +0900

    Music Cube Manager.cs追加、自動インスタンス

---
+++ Assets/Music Cube/MusicCubeManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicCubeManager : MonoBehaviour
+{
+    public GameObject cubeD;
+    public GameObject cubeE;
+    public GameObject cubeFsharp;
+    public GameObject cubeA;
+    public GameObject cubeH;
+
+    public float MinRange;
+    public float MaxRange;
+    public int cubeNum;
+
+    void Start()
+    {
+        for (int i = 1; i <= cubeNum; i++)
+        {
+            Instantiate(cubeD, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
+                Random.Range(MinRange, MaxRange)),
+                Quaternion.identity);
+
+            Instantiate(cubeE, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
+                Random.Range(MinRange, MaxRange)),
+                Quaternion.identity);
+
+            Instantiate(cubeFsharp, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
+                Random.Range(MinRange, MaxRange)),
+                Quaternion.identity);
+
+            Instantiate(cubeA, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
+                Random.Range(MinRange, MaxRange)),
+                Quaternion.identity);
+
+            Instantiate(cubeH, new Vector3(Random.Range(MinRange, MaxRange), 0.4f,
+                Random.Range(MinRange, MaxRange)),
+                Quaternion.identity);
+        }
+    }
+}
