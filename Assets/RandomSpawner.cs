4e44097af2641aa7541c984c2dc2590111aedad8
Hiroshi Moriyama <imet8335on2000@gmail.com>
2019-08-07 15:50:26 +0900

    Random Spawn.cs改作中

--- Assets/RandomSpawner.cs
+++ Assets/RandomSpawner.cs
@@ -8,21 +8,31 @@ public class RandomSpawner : MonoBehaviour
     private GameObject[] spawnPoints;   //生成位置格納
     public static Vector3 spawnPoint;   //生成位置格納
     public static Vector3 RespawnPoint; //前回の生成位置格納
+    public static Vector3 prefabPoint;
+
+    public int cubeNum;
 
 
     void Start()
     {
         spawnPoints = GameObject.FindGameObjectsWithTag("Respawn");
 
-        if (spawnPoints != null && spawnPoints.Length > 0)
+        for (int i = 1; i <= cubeNum; i++)
         {
-            while (spawnPoint == RespawnPoint)
+            if (spawnPoints != null && spawnPoints.Length > 0)
             {
-                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+                while (spawnPoint == RespawnPoint)
+                {
+                    spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+                    // インデントを代入
+                    // タグ名を変更
+                    // 座標を代入
+                }
+                // ifでタグ名が変更された場合Instantiate
+                Instantiate(respawnPrefab, spawnPoint, Quaternion.identity);
+                RespawnPoint = spawnPoint;
+                prefabPoint = respawnPrefab.transform.position;
             }
-
-            Instantiate(respawnPrefab, spawnPoint, transform.rotation);
-            RespawnPoint = spawnPoint;
         }
     }
 }
