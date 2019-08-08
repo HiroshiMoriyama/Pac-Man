b3a6d47dab54ec23d39f15866d0327fe00e02232
Hiroshi Moriyama <imet8335on2000@gmail.com>
2019-08-08 14:29:36 +0900

    MusicCubeランダム配置実装完了

--- Assets/RandomSpawner.cs
+++ Assets/RandomSpawner.cs
@@ -15,6 +15,9 @@ public class RandomSpawner : MonoBehaviour
     public static Vector3 RespawnPoint; //前回の生成位置格納
 
     public int cubeNum;     // Cube生成数(オブジェクトの数×cubeNum)
+    public int UpperMiddleCubeNum;
+    public int MiddleCubeNum;
+    public int LowerMiddleCubeNum;
 
 
     void Start()
@@ -133,6 +136,117 @@ public class RandomSpawner : MonoBehaviour
         spawnPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("SPointRight"));
 
         for (int i = 1; i <= cubeNum; i++)
+        {
+            if (spawnPoints != null && spawnPoints.Count > 0)
+            {
+                while (spawnPoint == RespawnPoint)
+                {
+                    int index = Random.Range(0, spawnPoints.Count);     // インデックス番号を取得
+                    spawnPoint = spawnPoints[index].transform.position;     // ランダムで選出された要素の座標を取得
+                    Instantiate(musicCubeD, spawnPoint, Quaternion.identity);    // Cubeをインスタンス
+                    spawnPoints.RemoveAt(index);    // ランダム選出されたインデックスの削除
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeE, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeFsharp, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeA, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeH, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+                }
+                RespawnPoint = spawnPoint;
+            }
+        }
+
+        spawnPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("SPoint UpperMiddle"));
+
+        for (int i = 1; i <= UpperMiddleCubeNum; i++)
+        {
+            if (spawnPoints != null && spawnPoints.Count > 0)
+            {
+                while (spawnPoint == RespawnPoint)
+                {
+                    int index = Random.Range(0, spawnPoints.Count);     // インデックス番号を取得
+                    spawnPoint = spawnPoints[index].transform.position;     // ランダムで選出された要素の座標を取得
+                    Instantiate(musicCubeD, spawnPoint, Quaternion.identity);    // Cubeをインスタンス
+                    spawnPoints.RemoveAt(index);    // ランダム選出されたインデックスの削除
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeE, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeFsharp, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeA, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeH, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+                }
+                RespawnPoint = spawnPoint;
+            }
+        }
+
+        spawnPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("SPoint Middle"));
+
+        for (int i = 1; i <= MiddleCubeNum; i++)
+        {
+            if (spawnPoints != null && spawnPoints.Count > 0)
+            {
+                while (spawnPoint == RespawnPoint)
+                {
+                    int index = Random.Range(0, spawnPoints.Count);     // インデックス番号を取得
+                    spawnPoint = spawnPoints[index].transform.position;     // ランダムで選出された要素の座標を取得
+                    Instantiate(musicCubeD, spawnPoint, Quaternion.identity);    // Cubeをインスタンス
+                    spawnPoints.RemoveAt(index);    // ランダム選出されたインデックスの削除
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeE, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeFsharp, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeA, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+
+                    index = Random.Range(0, spawnPoints.Count);
+                    spawnPoint = spawnPoints[index].transform.position;
+                    Instantiate(musicCubeH, spawnPoint, Quaternion.identity);
+                    spawnPoints.RemoveAt(index);
+                }
+                RespawnPoint = spawnPoint;
+            }
+        }
+
+        spawnPoints = new List<GameObject>(GameObject.FindGameObjectsWithTag("SPoint LowerMiddle"));
+
+        for (int i = 1; i <= LowerMiddleCubeNum; i++)
         {
             if (spawnPoints != null && spawnPoints.Count > 0)
             {
