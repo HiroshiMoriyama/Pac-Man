31308f48f39e5586609c7f988b07ca26dddf7749
Hiroshi Moriyama <imet8335on2000@gmail.com>
2019-08-09 15:48:26 +0900

    アイテム効果付与苦戦中

--- Assets/Item/ItemController.cs
+++ Assets/Item/ItemController.cs
@@ -14,12 +14,27 @@ public class ItemController : MonoBehaviour
 
     public IEnumerator loop(float second)
     {
+        GameObject skull = GameObject.Find("Skull");
+        GameObject enemy = GameObject.Find("Enemy Blue");
+
         while (true)
         {
             yield return new WaitForSeconds(second);
-            GameObject enemy = GameObject.Find("Enemy Blue");
+
+
+            // PCとenemyの座標が==の場合destroyも可能？上手くいかんかった
             Destroy(enemy);
             Debug.Log("IkeIke");
+
+
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
         }
     }
 }
